e93a793f89ec1ea56164415cf56c5f8969db5632
Anita Tiwari <anit8621@stud.kea.dk>
2022-02-18 00:39:46 +0100

    addingcss

--- BigFatString/script.js
+++ BigFatString/script.js
@@ -11,23 +11,77 @@ let pickedOption;
 let input;
 let output;
 let outputResult;
-window.addEventListener("DOMContentLoaded", start);
 
 
-function start(){
+
+
+const options= {
+1:function (string){
+    string =`${string.substring(0,1).toUpperCase()}${string.substring(1,string.length).toLowerCase()}`;
+    return string;
+},
+
+2:function (string){
+    const name =string.split(" ");
+    return name[0]
+},
+3: function (string) {
+    const names = string.split(" ");
+    return names[0].length;
+  },
+  4: function (string) {
+    const names = string.split(" ");
+    const middleArray = names.slice(1, names.length - 1);
+    const middleName = middleArray.join(" ");
+
+    const startPosition = string.indexOf(middleName);
+    const endPosition = startPosition + middleName.length - 1;
+
+    return `${middleName} Start: ${startPosition} End: ${endPosition}`;
+  },
+  5: function (string) {
+    if (string.endsWith(".jpg") || string.endsWith(".png")) {
+      return (string = "Image");
+    }
+    return (string = "not image");
+  },
+  6: function (string) {
+    string = "*".repeat(string.length);
+    return string;
+  },
+  7: function (string) {
+    string = `${string.substring(0, 2)}${string
+      .substring(2, 3)
+      .toUpperCase()}${string.substring(3)}`;
+    return string;
+  },
+  8: function (string) {
+    let upperCase = false;
+    let result = " ";
+
+    for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+      if (upperCase) {
+        char = char.toUpperCase();
+      }
+
+      upperCase = char === "-" || char === " ";
+      result += char;
+    }
+
+    return result;
+  },
+}
+
     generatebtn.addEventListener("click",generate)
-}
+
 function generate(){
-  
-   return  next.charAt(0).toUpperCase() + next.slice(1);
+    const input = document.querySelector("#inputtext").value;
+  const output = document.querySelector("#output");
+  const selected = option.options[option.selectedIndex].value;
 
+  output.value = options[selected](input);  
 }
-input=input1.value;
-output=output1.value;
-pickedOption=option.value;
-console.log(input)
-
-
 
 
 //   input = inputText.value;
